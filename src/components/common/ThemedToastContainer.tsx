7162713b671feca16dda2719f903bf2a1b0ac29a
Juan Camilo <juancamilo@MacBook-Air-de-JuanCamilo.local>
2026-09-08 07:23:14 -0500

    feat(ux): style toasts with AXORA design tokens and streamline feedback without redundancies

--- src/components/common/ThemedToastContainer.tsx
+++ src/components/common/ThemedToastContainer.tsx
@@ -1,5 +1,6 @@
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import './ThemedToastContainer.css'
 import { useTheme } from '../../hooks/useTheme.ts'
 
 export function ThemedToastContainer() {
